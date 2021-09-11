10453794a46dd3ac2c1e190011e7dc8a5d1a5751
hunter james <hunterevans344@gmail.com>
2021-09-11 11:23:45 -0700

    fix: onlogin bv

--- index.js
+++ index.js
@@ -210,20 +210,6 @@ function onReady() {
           json.sessionId,
           consts.eventNames.sessionEventFromPartition
         );
-
-        elements[json.sessionId].webContents.on(
-          "login",
-          (event, request, authInfo, callback) => {
-            event.preventDefault();
-            registerCallback(
-              json,
-              consts.callbackNames.webContentsLogin,
-              { authInfo: authInfo, request: request },
-              consts.eventNames.webContentsEventLogin,
-              callback
-            );
-          }
-        );
         break;
       case consts.eventNames.sessionCmdCloseAllConnections:
         elements[json.targetID].closeAllConnections().then(() => {
@@ -1083,6 +1069,20 @@ function browserViewCreateFinish(json) {
       consts.eventNames.browserViewEventDidFinishLoad
     );
   });
+
+  elements[json.targetID].webContents.on(
+    "login",
+    (event, request, authInfo, callback) => {
+      event.preventDefault();
+      registerCallback(
+        json,
+        consts.callbackNames.webContentsLogin,
+        { authInfo: authInfo, request: request },
+        consts.eventNames.webContentsEventLogin,
+        callback
+      );
+    }
+  );
 }
 
 function registerCallback(json, k, e, n, c) {
